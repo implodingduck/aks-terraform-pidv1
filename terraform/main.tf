4a613b420d7cddaa90915b390cd600192554bc26
implodingduck <zsek320@gmail.com>
2022-03-18 14:19:26 -0500

    all the things

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,6 +22,7 @@ provider "azurerm" {
 
 locals {
   func_name = "func${random_string.unique.result}"
+  cluster_name = "aksterra-pidv1-${random_string.unique.result}"
   loc_for_naming = lower(replace(var.location, " ", ""))
   gh_repo = replace(var.gh_repo, "implodingduck/", "")
   tags = {
@@ -53,4 +54,126 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-${local.gh_repo}-${random_string.unique.result}-${local.loc_for_naming}"
   location = var.location
   tags = local.tags
+}
+
+resource "azurerm_virtual_network" "default" {
+  name                = "${local.cluster_name}-vnet-${local.loc_for_naming}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+
+  tags = local.tags
+}
+
+resource "azurerm_subnet" "default" {
+  name                 = "default-subnet-${local.loc_for_naming}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+resource "azurerm_subnet" "cluster" {
+  name                 = "${local.cluster_name}-subnet-${local.loc_for_naming}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes     = ["10.0.2.0/23"]
+
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = local.cluster_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "quackersbank" #local.cluster_name
+  kubernetes_version  = "1.21.2"
+  default_node_pool {
+    name            = "default"
+    node_count      = 1
+    vm_size         = "Standard_D2_v2"
+    os_disk_size_gb = "128"
+    vnet_subnet_id  = azurerm_subnet.cluster.id
+
+
+  }
+  network_profile {
+    network_plugin     = "azure"
+    network_policy     = "azure"
+    service_cidr       = "10.255.252.0/22"
+    dns_service_ip     = "10.255.252.10"
+    docker_bridge_cidr = "172.17.0.1/16"
+  }
+
+  role_based_access_control {
+    enabled = true
+    
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+  
+  addon_profile {
+    oms_agent {
+      enabled                    = true
+      log_analytics_workspace_id = data.azurerm_log_analytics_workspace.default.id
+    }
+  }
+
+  tags = local.tags
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "acr${local.cluster_name}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard"
+
+}
+
+resource "azurerm_role_assignment" "acrpull_role" {
+  scope                            = azurerm_container_registry.acr.id
+  role_definition_name             = "AcrPull"
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+}
+
+resource "azurerm_application_insights" "app" {
+  name                = "${local.cluster_name}-app-insights"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  application_type    = "other"
+  workspace_id        = data.azurerm_log_analytics_workspace.default.id
+}
+
+resource "azurerm_user_assigned_identity" "uai" {
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  name = "pid-via-terraform-fun"
+}
+
+resource "null_resource" "enable-pod-identity"{
+  depends_on = [
+    azurerm_kubernetes_cluster.aks,
+    azurerm_user_assigned_identity.uai
+  ]
+  triggers = {
+    index = azurerm_kubernetes_cluster.aks.private_fqdn
+  }
+  provisioner "local-exec" {
+    command     = "az aks update -g ${azurerm_resource_group.rg.name} -n ${local.cluster_name} --enable-pod-identity"
+  }
+}
+
+resource "null_resource" "add-pod-identity"{
+  depends_on = [
+    azurerm_kubernetes_cluster.aks,
+    azurerm_user_assigned_identity.uai,
+    null_resource.enable-pod-identity
+  ]
+  triggers = {
+    index = azurerm_user_assigned_identity.uai.principal_id
+  }
+  provisioner "local-exec" {
+    command     = "az aks pod-identity add -g ${azurerm_resource_group.rg.name} --cluster-name ${local.cluster_name} --namespace default  --name pid-via-terraform-fun --identity-resource-id ${azurerm_user_assigned_identity.uai.id}"
+  }
 }
