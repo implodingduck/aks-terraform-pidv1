619fb2dd14e24230d512ad34756b421700e6618d
implodingduck <zsek320@gmail.com>
2022-03-18 14:32:02 -0500

    using double quotes

--- terraform/main.tf
+++ terraform/main.tf
@@ -127,7 +127,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "acr${replace(local.cluster_name, '-','')}"
+  name                = "acr${replace(local.cluster_name, "-","")}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Standard"
